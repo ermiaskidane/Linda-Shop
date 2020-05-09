d1fa3c13b3bd3758a497ebe641f1b9210475c7a2
ermias kidane <hadeklte21@gmail.com>
2020-05-09 17:04:59 +0100

    Updates

---
+++ precache-manifest.0eb616082e831c016923e0f853bfb98b.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d79b1801762119cdb26b2b60c60599a",
+    "url": "/Linda-Shop/index.html"
+  },
+  {
+    "revision": "8bad3c2d66730a0b3440",
+    "url": "/Linda-Shop/static/css/main.515dffbb.chunk.css"
+  },
+  {
+    "revision": "a80dbbf4d1efaf73a322",
+    "url": "/Linda-Shop/static/js/2.b3f78a7b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Linda-Shop/static/js/2.b3f78a7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bad3c2d66730a0b3440",
+    "url": "/Linda-Shop/static/js/main.48bb1145.chunk.js"
+  },
+  {
+    "revision": "2e31bc6c5c678fa3efcb",
+    "url": "/Linda-Shop/static/js/runtime-main.fd4f8fe1.js"
+  },
+  {
+    "revision": "d83f6a6d419a2b8a21fb0b803f2d2b67",
+    "url": "/Linda-Shop/static/media/blog-1.d83f6a6d.jpg"
+  },
+  {
+    "revision": "2d1216970147c0d60aa97b81450a39b9",
+    "url": "/Linda-Shop/static/media/blog-2.2d121697.jpg"
+  },
+  {
+    "revision": "9db8a42479d03d314d58e58b3ced2844",
+    "url": "/Linda-Shop/static/media/download.9db8a424.webp"
+  },
+  {
+    "revision": "05103b09c3671db01d30a816c9dd7e47",
+    "url": "/Linda-Shop/static/media/fashion-1.05103b09.jpg"
+  },
+  {
+    "revision": "686d3134e410bde2c802229a768ab6bf",
+    "url": "/Linda-Shop/static/media/fashion-10.686d3134.webp"
+  },
+  {
+    "revision": "ef89b62037d1bd5ed61e7077db654d63",
+    "url": "/Linda-Shop/static/media/fashion-14.ef89b620.webp"
+  },
+  {
+    "revision": "df2845f203504d6d494a07133183f5a7",
+    "url": "/Linda-Shop/static/media/fashion-17.df2845f2.webp"
+  },
+  {
+    "revision": "e76eae40eb16735ae3c4434e7468367b",
+    "url": "/Linda-Shop/static/media/fashion-18.e76eae40.webp"
+  },
+  {
+    "revision": "4b0292dab6bfbd279f530abc847abe4f",
+    "url": "/Linda-Shop/static/media/fashion-20.4b0292da.webp"
+  },
+  {
+    "revision": "619456d2826762b67c9a6da5bc1136ae",
+    "url": "/Linda-Shop/static/media/fashion-3.619456d2.jpg"
+  },
+  {
+    "revision": "19bdec4e8d4e940016000b09fd2d672e",
+    "url": "/Linda-Shop/static/media/fashion-4.19bdec4e.jpg"
+  },
+  {
+    "revision": "9a80edd082773654c6370f0206fcfe16",
+    "url": "/Linda-Shop/static/media/fashion-5.9a80edd0.jpg"
+  },
+  {
+    "revision": "fb05698f5d7eeed9424f3bf91ff77631",
+    "url": "/Linda-Shop/static/media/fashion-6.fb05698f.jpg"
+  },
+  {
+    "revision": "2837ea0aa1ce2878a531c555600fb1dc",
+    "url": "/Linda-Shop/static/media/fashion-9.2837ea0a.webp"
+  },
+  {
+    "revision": "411592854f48320636622cc4b41aef36",
+    "url": "/Linda-Shop/static/media/person (1).41159285.jpg"
+  },
+  {
+    "revision": "ca3c40324fc4440fc7eaecab53d90796",
+    "url": "/Linda-Shop/static/media/snow.ca3c4032.jpg"
+  },
+  {
+    "revision": "d6916c7418f0dd07a7d693b510417d6b",
+    "url": "/Linda-Shop/static/media/sprite.d6916c74.svg"
+  },
+  {
+    "revision": "ad7286c48906ed2a8b7d18eeb804e141",
+    "url": "/Linda-Shop/static/media/tamara-bellis-10.ad7286c4.jpg"
+  },
+  {
+    "revision": "af597c59701030f041eb85a355175460",
+    "url": "/Linda-Shop/static/media/tamara-bellis-6.af597c59.jpg"
+  },
+  {
+    "revision": "4b9e38a268a18a62531c6163a15e798c",
+    "url": "/Linda-Shop/static/media/tamara-bellis-9.4b9e38a2.jpg"
+  }
+]);
